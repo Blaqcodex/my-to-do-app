4f0c83ff0714fcf5b62ad136bc42d2f9ef0aea34
Thabiso <220024664@stu.ukzn.ac.za>
2026-02-15 15:51:02 +0200

    Add TodoList component for rendering task collection

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,19 @@
+import TodoItem from "./TodoItem";
+
+function TodoList({ tasks, deleteTask, toggleComplete, editTask }) {
+  return (
+    <ul>
+      {tasks.map((task) => (
+        <TodoItem
+          key={task.id}
+          task={task}
+          deleteTask={deleteTask}
+          toggleComplete={toggleComplete}
+          editTask={editTask}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
